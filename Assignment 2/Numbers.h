bc7f7b7205af943942503dd5d868fa1178c347eb
Jghaddad123 <51768855+Jghaddad123@users.noreply.github.com>
2020-06-14 13:41:20 -0700

    Create Numbers.h

---
+++ Assignment 2/Numbers.h
@@ -0,0 +1,39 @@
+//
+// Created by Beccs on 6/8/20.
+//
+
+#ifndef MULTITHREADING_NUMBERS_H
+#define MULTITHREADING_NUMBERS_H
+
+
+class Numbers {
+private:
+    int size;
+    int *myarray;
+    int sum;
+
+public:
+    // Default constructor
+    Numbers();
+    // Parameterized constructor
+    Numbers(int size);
+    // Destructor
+    ~Numbers();
+    // Getters and setters
+    int getSize() const;
+
+    void setSize(int value);
+
+    int *getMyarray() const;
+
+    void setMyarray(int arrSize);
+
+    int getSum() const;
+
+    void setSum(int value);
+
+};
+
+
+
+#endif //MULTITHREADING_NUMBERS_H
